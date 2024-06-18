25187544ee4f76758cc3808b5a6e6e570b92e9d7
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-06-17 22:48:56 -0500

    Add a second function swapText

--- iceCream.js
+++ iceCream.js
@@ -4,3 +4,12 @@ function secondFav() {
 
 }
 myIceCreamButton.onclick = secondFav;
+
+function swapText() {
+    let x = document.getElementById("textSwap");
+    if (x.innerHTML === "Hiya Pal!") {
+        x.innerHTML = "Swapped You!";
+    } else {
+        x.innerHTML = "Hiya Pal!";
+    }
+}
