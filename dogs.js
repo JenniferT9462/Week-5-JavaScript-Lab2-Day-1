77dbad15d7a15da4facc7dca0bdaf79026bbbd0e
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-06-18 00:11:49 -0500

    Add image to my function

--- dogs.js
+++ dogs.js
@@ -1,5 +1,8 @@
 function favDog() {
     myDogHeader.innerHTML = "It's Daisy!";
     myDogParagraph.innerHTML = "Daisy the Amazing Doggie!!";
+    // Took me forever to figure out but this displays the hidden img
+    daisyImage.style.display = "block";
+
 }
 myDogButton.onclick = favDog;
