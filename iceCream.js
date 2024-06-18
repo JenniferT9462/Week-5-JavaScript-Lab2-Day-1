44fca07eaf09eae00e891ec7d34a29307d72b1c5
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-06-18 00:13:48 -0500

    Add a second function

--- iceCream.js
+++ iceCream.js
@@ -4,7 +4,7 @@ function secondFav() {
 
 }
 myIceCreamButton.onclick = secondFav;
-
+// Wanted to figure out how to loop through with one button...or reset
 function swapText() {
     let x = document.getElementById("textSwap");
     if (x.innerHTML === "Hiya Pal!") {
